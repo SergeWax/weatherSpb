59806b62f3defb5479d6c166dbc77279f198728e
sergewax <whitewaxbox@gmail.com>
2022-10-28 01:14:28 +0400

    first commit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Parser {
+
+    private static Document spbPage() throws IOException {
+        String url = "https://pogoda.spb.ru/";
+        Document newDoc = Jsoup.parse(new URL(url), 3000 );
+        return newDoc;
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println(spbPage());
+    }
+
 }
