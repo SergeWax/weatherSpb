a843c00008f78078e1d6dcb652c523d95395e1cd
sergewax <whitewaxbox@gmail.com>
2022-10-30 20:47:46 +0300

    fourth commit.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,7 +4,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,15 +25,27 @@ public class Parser {
         }
         throw new Exception("Can't extract date from string");
     }
-
-    private static void printFomValues(Elements values, int index){
-        Element valueLine = values.get(index);
-        for (Element td : valueLine.select("td")){
-            System.out.println(td.text() + "    ");
+    /////////////////
+    private static int printPartValues(Elements values, int index) {
+        int iterationCount = 4;
+        if (index == 0) {
+            Element valueLn = values.get(3);
+            boolean isMorning = valueLn.text().contains("Утро");
+            if (isMorning) {
+                iterationCount = 3;
+            }// все ок
         }
-        System.out.println();
-    }
-
+//////////
+        for (int i = 0; i < iterationCount; i++) {
+            Element valueLine = values.get(index + 1);
+            for (Element td : valueLine.select("td")) {
+                System.out.print(td.text() + "    ");
+            }
+            System.out.println();
+        }
+        return iterationCount;
+    } // ok
+    //////////
     public static void main(String[] args) throws Exception {
         Document page = spbPage();
         //css query language
@@ -42,14 +53,14 @@ public class Parser {
         Elements names = tableWth.select("tr[class=wth]");
         Elements values = tableWth.select("tr[valign=top]");
         int index = 0; //на каком значении мы сейчас находимся
-
         for (Element name : names) {
             String dateString = name.select("th[id=dt]").text();
             String date = getDateFromString(dateString);
-            System.out.println(date);
-            printFomValues(values, index);
+            System.out.println(date + "    Явления    Температура    Давл    Влажность    Ветер");
+            int iterationCount = printPartValues(values, index);
+            //все ок
+            index = index + iterationCount;
             //Регулярные выражения необходимы для того что бы выбрать из строки необходимую часть текста
-
 
         }
     }
